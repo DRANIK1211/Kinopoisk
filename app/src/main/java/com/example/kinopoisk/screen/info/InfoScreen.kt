29e8ec799337668fbb844ec55e2feec2a6c7b4fb
DRANIK1211 <andreiduzhinin@yandex.ru>
2025-01-24 16:10:07 +0700

    add search

--- app/src/main/java/com/example/kinopoisk/screen/info/InfoScreen.kt
+++ app/src/main/java/com/example/kinopoisk/screen/info/InfoScreen.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -44,7 +50,7 @@ data class InfoFilm(
 
 
 @Composable
-fun InfoScreen(film: InfoFilm){
+fun InfoScreen(film: InfoFilm) {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -56,7 +62,7 @@ fun InfoScreen(film: InfoFilm){
             contentScale = ContentScale.FillWidth
         )
         Column(
-            modifier = Modifier.padding(start=52.dp, end=52.dp, top=10.dp),
+            modifier = Modifier.padding(start = 52.dp, end = 52.dp, top = 10.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
@@ -66,14 +72,14 @@ fun InfoScreen(film: InfoFilm){
                 fontWeight = FontWeight(700)
             )
             Text(
-                text = film.nameOriginal +" "+ film.age.substring(3),
+                text = film.nameOriginal + " " + film.age.substring(3),
                 modifier = Modifier.padding(bottom = 5.dp),
                 fontSize = 8.sp,
                 fontWeight = FontWeight(500)
             )
 
             Text(
-                text = if(film.type == "TV_SERIES") "О сериале" else "О фильме",
+                text = if (film.type == "TV_SERIES") "О сериале" else "О фильме",
                 modifier = Modifier.padding(bottom = 5.dp),
                 fontSize = 14.sp,
                 fontWeight = FontWeight(700)
@@ -85,7 +91,7 @@ fun InfoScreen(film: InfoFilm){
                     .fillMaxWidth()
                     .padding(bottom = 5.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
-            ){
+            ) {
                 Text(
                     "Год производства",
                     fontSize = 10.sp,
@@ -102,7 +108,7 @@ fun InfoScreen(film: InfoFilm){
                     .fillMaxWidth()
                     .padding(bottom = 5.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
-            ){
+            ) {
                 Text(
                     "Страна",
                     fontSize = 10.sp,
@@ -119,7 +125,7 @@ fun InfoScreen(film: InfoFilm){
                     .fillMaxWidth()
                     .padding(bottom = 5.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
-            ){
+            ) {
                 Text(
                     "Жанры",
                     fontSize = 10.sp,
@@ -141,19 +147,163 @@ fun InfoScreen(film: InfoFilm){
                     .fillMaxWidth()
                     .padding(bottom = 5.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
-            ){
+            ) {
                 Text(
                     "Время",
                     fontSize = 10.sp,
                     fontWeight = FontWeight(300)
                 )
                 Text(
-                    text = film.filmLength.toString() + "мин. / " + len,
+                    text = film.filmLength.toString() + " мин. / " + len,
                     fontSize = 10.sp,
                     fontWeight = FontWeight(300)
                 )
             }
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 20.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    "Слоган",
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight(300)
+                )
+                Text(
+                    text = film.slogan,
+                    fontSize = 10.sp,
+                    fontWeight = FontWeight(300)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 5.dp),
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f)
+                        .padding(bottom = 5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.rating4),
+                        contentDescription = null,
+                        modifier = Modifier.padding(bottom = 10.dp).height(32.dp).width(93.dp)
+                    )
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 5.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Image(
+                            painter = painterResource(R.drawable.g8),
+                            contentDescription = null,
+                            modifier = Modifier.height(22.dp).width(22.dp)
+                        )
+                        Text(
+                            text = "${film.ratingI}/10",
+                            fontSize = 10.sp,
+                            fontWeight = FontWeight(300)
+                        )
+                        Text(
+                            text = film.ratingIVC.toString(),
+                            fontSize = 10.sp,
+                            fontWeight = FontWeight(300)
+                        )
+                    }
+                }
+                Column (
+                    modifier = Modifier.weight(1f)
+                        .padding(bottom = 5.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ){
+                    Image(
+                        painter = painterResource(R.drawable.rating5),
+                        contentDescription = null,
+                        modifier = Modifier.padding(bottom = 10.dp).height(32.dp).width(93.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 5.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.g8),
+                            contentDescription = null,
+                            modifier = Modifier.height(22.dp).width(22.dp)
+                        )
+                        Text(
+                            text = "${film.ratingK}/10",
+                            fontSize = 10.sp,
+                            fontWeight = FontWeight(300)
+                        )
+                        Text(
+                            text = film.ratingKVC.toString(),
+                            fontSize = 10.sp,
+                            fontWeight = FontWeight(300)
+                        )
+                    }
+                }
+
+            }
+
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 10.dp, horizontal = 15.dp),
+                thickness = 1.dp,
+                color = Color(189, 189, 189)
+            )
+
+            Text(
+                text = "Описание",
+                fontSize = 10.sp,
+                fontWeight = FontWeight(700),
+                modifier = Modifier.padding(bottom = 5.dp)
+            )
+
+            Text(
+                text = film.description,
+                fontSize = 10.sp,
+                fontWeight = FontWeight(300)
+            )
+
+            Button(
+                onClick = {},
+                colors = ButtonColors(
+                    containerColor = Color(100, 100, 100),
+                    contentColor = Color.White,
+                    disabledContentColor = Color(100, 100, 100),
+                    disabledContainerColor = Color.White,
+
+                )
+            ) {
+                Text("Открыть на сайте Кинопоиск", fontSize = 10.sp, fontWeight = FontWeight(700))
+
+            }
+            Button(
+                onClick = {},
+                colors = ButtonColors(
+                    containerColor = Color(100, 100, 100),
+                    contentColor = Color.White,
+                    disabledContentColor = Color(100, 100, 100),
+                    disabledContainerColor = Color.White,
+
+                    )
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.asd),
+                    contentDescription = null,
+                    modifier = Modifier.height(33.dp).width(33.dp).padding(end = 10.dp)
+                )
+                Text("Добавить в избранное", fontSize = 10.sp, fontWeight = FontWeight(700))
+
+            }
         }
     }
 }
@@ -161,7 +311,7 @@ fun InfoScreen(film: InfoFilm){
 
 @Preview(showSystemUi = true, device = Devices.PIXEL_4)
 @Composable
-fun InfoScreenPreview(){
+fun InfoScreenPreview() {
     InfoScreen(
         film = InfoFilm(
             image = "https://kinopoiskapiunofficial.tech/images/posters/kp_small/4470907.jpg",
